b93a13c652c26519cec8ff3420fa45dc879a54eb
Antoine Musso <hashar@free.fr>
2014-07-21 21:45:26 +0200

    Basic jshint fixes
    * ignore webplatform/webplatform.js since that it is a third party
      libary
    * replace tabs with spaces
    * provide a default .jshintrc file
    
    Change-Id: I5502966c90f53e92b0a7097e1cf2552a56b45fdc

--- webplatform/webplatform.js
+++ webplatform/webplatform.js
@@ -4,7 +4,7 @@ function setupSearchField() {
 
     searchInput.focus(function () {
         if (searchInput.val() == defaultValue) {
-            searchInput.val('');    
+            searchInput.val('');
         }
     });
 
@@ -29,44 +29,44 @@ function setupDropdown() {
 function init() {
     setupSearchField();
     setupDropdown();
-    
+
     if (document.querySelectorAll && document.body.addEventListener) {
-    	var dropdowns = document.querySelectorAll('.dropdown');
-    	
-    	for (var i=0, dropdown; dropdown=dropdowns[i++];) {
-    		dropdown.addEventListener('focus', function () {
-    			this.className += ' focus';
-    		}, true);
-    		
-    		dropdown.addEventListener('blur', function () {
-    			this.className = this.className.replace(/\s+focus\b/, ' ');
-    		}, true);
-    	}
-    	
-    	// Syntax highlighting for examples with a language
-    	var langs = document.querySelectorAll('.example > p > .language');
-    	
-    	for (var i=0, lang; lang = langs[i++];) {
-    		var pre = lang.parentNode.parentNode.querySelector('pre');
-    		
-    		var code = document.createElement('code');
-    		code.className = 'language-' + {
-    			'JavaScript': 'javascript',
-    			'HTML': 'markup',
-    			'CSS': 'css'
-    		}[lang.textContent];
-    		
-    		code.innerHTML = pre.innerHTML;
-    		pre.innerHTML = '';
-    		pre.appendChild(code);
-    	}
-    	
-    	var prism = document.createElement('script');
-    	prism.src = '/t/skins/webplatform/prism.js';
-    	document.head.appendChild(prism);
-    	prism.onload = function () {
-    		window.Prism && Prism.highlightAll();
-    	}
+        var dropdowns = document.querySelectorAll('.dropdown');
+
+        for (var i=0, dropdown; dropdown=dropdowns[i++];) {
+            dropdown.addEventListener('focus', function () {
+                this.className += ' focus';
+            }, true);
+
+            dropdown.addEventListener('blur', function () {
+                this.className = this.className.replace(/\s+focus\b/, ' ');
+            }, true);
+        }
+
+        // Syntax highlighting for examples with a language
+        var langs = document.querySelectorAll('.example > p > .language');
+
+        for (var i=0, lang; lang = langs[i++];) {
+            var pre = lang.parentNode.parentNode.querySelector('pre');
+
+            var code = document.createElement('code');
+            code.className = 'language-' + {
+                'JavaScript': 'javascript',
+                'HTML': 'markup',
+                'CSS': 'css'
+            }[lang.textContent];
+
+            code.innerHTML = pre.innerHTML;
+            pre.innerHTML = '';
+            pre.appendChild(code);
+        }
+
+        var prism = document.createElement('script');
+        prism.src = '/t/skins/webplatform/prism.js';
+        document.head.appendChild(prism);
+        prism.onload = function () {
+            window.Prism && Prism.highlightAll();
+        }
     }
 }
 
